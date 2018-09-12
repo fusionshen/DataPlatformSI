1b3d000b42da2378df202677a3566290bb7d55c4
fusionshen <365190287@qq.com>
2018-09-12 14:53:34 +0800

    完成下载模块dll接口

--- src/DataPlatformSI.WebAPI/Controllers/ModulesController.cs
+++ src/DataPlatformSI.WebAPI/Controllers/ModulesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using DataPlatformSI.DomainClasses;
@@ -13,6 +14,8 @@ namespace DataPlatformSI.WebAPI.Controllers
     //[EnableCors("CorsPolicy")]
     public class ModulesController : Controller
     {
+        private readonly string _moduleDirectory = $"{System.AppContext.BaseDirectory}/Downloads/Modules";
+
         ModuleMetadata[] modules = new ModuleMetadata[]
         {
             new ModuleMetadata { Id = 1, ModuleName = "BasicModule",Checksum ="82240e32a858fbfe5e77b0c920b68e2c", ModuleType="DataPlatformRI.Modules.Basic.BasicModule, DataPlatformRI.Modules.Basic, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" }
@@ -39,13 +42,13 @@ namespace DataPlatformSI.WebAPI.Controllers
 
         // POST: api/Modules
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] ModuleMetadata value)
         {
         }
 
         // PUT: api/Modules/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] ModuleMetadata value)
         {
         }
 
@@ -54,5 +57,31 @@ namespace DataPlatformSI.WebAPI.Controllers
         public void Delete(int id)
         {
         }
+
+        /// <summary>
+        /// 下载dll
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        //Get: api/Modules/1/Content
+        [HttpGet("{id}/Content")]
+        public IActionResult GetContentById(int id)
+        {
+            if (modules.FirstOrDefault(m => m.Id == id) == null)
+            {
+                return NotFound();
+            }
+            var addrUrl = $"{_moduleDirectory}/{GetFileNameFromModuleType(modules.FirstOrDefault(m => m.Id == id).ModuleType)}";
+            if (!System.IO.File.Exists(addrUrl))
+            {
+                return NotFound();
+            }
+            var stream = System.IO.File.OpenRead(addrUrl);
+            //return File(stream, "application/vnd.android.package-archive", Path.GetFileName(addrUrl));
+            return Ok(stream);
+        }
+
+        private string GetFileNameFromModuleType(string moduleType) => $"{moduleType.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)[1]}.dll";
+
     }
 }
