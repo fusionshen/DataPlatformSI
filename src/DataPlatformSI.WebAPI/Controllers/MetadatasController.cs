75784c3f23fcbad0cb7ad0d05996701907f6d15a
fusionshen <365190287@qq.com>
2018-09-28 18:08:51 +0800

    添加若干示例json schema

--- src/DataPlatformSI.WebAPI/Controllers/MetadatasController.cs
+++ src/DataPlatformSI.WebAPI/Controllers/MetadatasController.cs
@@ -24,7 +24,29 @@ namespace DataPlatformSI.WebAPI.Controllers
             new MetadataItem { Id = 3, Name = "3_supplier_供应商", AliasName="供应商",IconName="AccountStar", Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
             new MetadataItem { Id = 4, Name = "4_customer_客户", AliasName="客户",IconName="AccountSwitch", Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
             new MetadataItem { Id = 5, Name = "5_contact_往来单位", AliasName="往来单位",IconName="Contacts", Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
-            new MetadataItem { Id = 6, Name = "6_testmetadata_测试元数据", AliasName="测试元数据", IconName="TestTube",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"}
+            new MetadataItem { Id = 6, Name = "6_testmetadata_测试元数据", AliasName="测试元数据", IconName="TestTube",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            //ref:https://mozilla-services.github.io/react-jsonschema-form/
+            new MetadataItem { Id = 7, Name = "7_simple_Simple", AliasName="Simple", IconName="Clippy",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 8, Name = "8_nested_Nested", AliasName="Nested", IconName="NestProtect",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 9, Name = "9_arrays_Arrays", AliasName="Arrays", IconName="CodeArray",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 10, Name = "10_numbers_Numbers", AliasName="Numbers", IconName="FormatListNumbers",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 11, Name = "11_widgets_Widgets", AliasName="Widgets", IconName="Widgets",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 12, Name = "12_ordering_Ordering", AliasName="Ordering", IconName="ReorderHorizontal",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 13, Name = "13_references_References", AliasName="References", IconName="AlarmLight",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 14, Name = "14_custom_Custom", AliasName="Custom", IconName="ImageFilterCenterFocusWeak",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 15, Name = "15_errors_Errors", AliasName="Errors", IconName="Ferry",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 16, Name = "16_large_Large", AliasName="Large", IconName="GridLarge",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 17, Name = "17_date&time_Date&time", AliasName="Date & time", IconName="Timer",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 18, Name = "18_validation_Validation", AliasName="Validation", IconName="Quicktime",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 19, Name = "19_files_Files", AliasName="Files", IconName="File",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 20, Name = "20_single_Single", AliasName="Single", IconName="SkypeBusiness",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 21, Name = "21_customArray_CustomArray", AliasName="Custom Array", IconName="ViewArray",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 22, Name = "22_customObject_CustomObject", AliasName="Custom Object", IconName="Pokeball",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 23, Name = "23_alternatives_Alternatives", AliasName="Alternatives", IconName="Football",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 24, Name = "24_propertyDependencies_PropertyDependencies", AliasName="Property Dependencies", IconName="Airballoon",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            new MetadataItem { Id = 25, Name = "25_schemaDependencies_SchemaDependencies", AliasName="Schema Dependencies", IconName="CodeTagsCheck",Checksum ="82240e32a858fbfe5e77b0c920b68e2c"},
+            //ref:https://github.com/RSuter/VisualJsonEditor
+            new MetadataItem { Id = 26, Name = "26_jsonEditor_JsonEditor", AliasName="Json Editor", Checksum ="82240e32a858fbfe5e77b0c920b68e2c"}
         };
 
         // GET: api/Metadatas
