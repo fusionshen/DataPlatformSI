47dcec93bdff4ea708a548c7cd3ad320e456fa17
fusionshen <365190287@qq.com>
2019-01-08 14:41:52 +0800

    新增MDM模块

---
+++ src/DataPlatformSI.WebAPI/Controllers/MDMController.cs
@@ -0,0 +1,50 @@
+﻿using DataPlatformSI.Services.Identity;
+using DataPlatformSI.ViewModels.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace DataPlatformSI.WebAPI.Controllers
+{
+    [Authorize(Policy = ConstantPolicies.DynamicPermission)]
+    [Area("DataPlatformRI.Modules.MDM")]
+    [DisplayName("主数据管理模块")]
+    public class MDMController : Controller
+    {
+        [DisplayName("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RoleViewModel model)
+        {
+            return View(model);
+        }
+
+        [DisplayName("Books")]
+        public IActionResult Books()
+        {
+            return View(viewName: "Index");
+        }
+
+        [DisplayName("Users")]
+        public IActionResult Users()
+        {
+            return View(viewName: "Index");
+        }
+
+        [DisplayName("BooksGiven")]
+        public IActionResult BooksGiven()
+        {
+            return View(viewName: "Index");
+        }
+
+        [DisplayName("BooksMissings")]
+        public IActionResult BooksMissings()
+        {
+            return View(viewName: "Index");
+        }
+    }
+}
